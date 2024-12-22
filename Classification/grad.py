fdf9b65e4394eed9418f473a085ff492ed9fc30a
Jinxu-Lin <jlin8272@uni.sydney.edu.au>
2024-12-22 17:10:49 +1100

    train

--- Classification/grad.py
+++ Classification/grad.py
@@ -5,6 +5,7 @@ import os
 
 import torch
 from torch import optim
+from torch.func import functional_call, vmap, grad 
 
 from trak.projectors import ProjectionType, AbstractProjector, CudaProjector
 
@@ -17,6 +18,12 @@ dataset_num_classes = {
     'cifar2': 2,
     'cifar10': 10,
     'imagenet': 1000
+}
+
+dataset_len = {
+    'cifar2': 10000,
+    'cifar10': 50000,
+    'imagenet': 1281167
 }
 
 dataset_loader = {
@@ -33,6 +40,35 @@ models = {
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def compute_f(model, params, buffers, inputs, labels):
+    inputs = inputs.unsqueeze(0)
+    labels = labels.unsqueeze(0)
+   
+    predictions = functional_call(model, (params, buffers), args=inputs)
+    ####
+    f = torch.nn.CrossEntropyLoss()(predictions.float(), labels.float())
+    ####
+    return f  
+
+
+def vectorize_and_ignore_buffers(g, params_dict=None):
+    """
+    gradients are given as a tuple :code:`(grad_w0, grad_w1, ... grad_wp)` where
+    :code:`p` is the number of weight matrices. each :code:`grad_wi` has shape
+    :code:`[batch_size, ...]` this function flattens :code:`g` to have shape
+    :code:`[batch_size, num_params]`.
+    """
+    batch_size = len(g[0])
+    out = []
+    if params_dict is not None:
+        for b in range(batch_size):
+            out.append(torch.cat([x[b].flatten() for i, x in enumerate(g) if is_not_buffer(i, params_dict)]))
+    else:
+        for b in range(batch_size):
+            out.append(torch.cat([x[b].flatten() for x in g]))
+    return torch.stack(out)
 
 
 def parseArgs():
@@ -92,12 +128,14 @@ def main(args):
     device = torch.device("cpu")
 
     # load dataset
-    train_loader = dataset_loader[args.dataset].get_train_loader(
-        args,
-    )
-    test_loader = dataset_loader[args.dataset].get_test_loader(
-        args,
-    )
+    if 'idx-train.pkl' in args.index_path:
+        loader = dataset_loader[args.dataset].get_train_loader(
+            args,
+        )
+    else:
+        loader = dataset_loader[args.dataset].get_test_loader(
+            args,
+        )
 
     # load model
     num_classes = dataset_num_classes[args.dataset]
@@ -112,14 +150,40 @@ def main(args):
         proj_type=ProjectionType.normal,
         device='cuda:0'
     )
+
+    # get params and buffers
+    params = {k: v.detach() for k, v in model.named_parameters() if v.requires_grad==True}
+    buffers = {k: v.detach() for k, v in model.named_buffers() if v.requires_grad==True}
+
+    # Initialize save np array
+    if 'idx-train.pkl' in args.index_path:
+        filename = os.path.join('./saved/grad/train-grad-{}-{}-{}.npy'.format(
+            args.model, args.model_name, args.dim
+        ))
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    dstore_keys = np.memmap(filename, 
+                            dtype=np.float32, 
+                            mode='w+', 
+                            shape=(dataset_len[args.dataset], args.dim)) 
+    
     
     # model evaluation for gradient computation
     model.to(device)
     model.train()
 
-    for batch_idx, batch in enumerate(train_loader):
+    ft_compute_grad = grad(compute_f)
+    ft_compute_sample_grad = vmap(ft_compute_grad, 
+                              in_dims=(None, None, 0, 0, 0, 
+                                       ),
+                             )
+
+    for batch_idx, batch in enumerate(loader):
         print("batch_idx: ", batch_idx)
-        data, labels = batch["input"].to(device), batch["label"].to(device)
+        inputs, labels = batch["input"].to(device), batch["label"].to(device)
+
+        ft_per_sample_grads = ft_compute_sample_grad(model, params, buffers, inputs, labels)
+        ft_per_sample_grads = vectorize_and_ignore_buffers(list(ft_per_sample_grads.values()))
 
 
 
