6a3d3922d793d593d7db2f86c876d17d6cc81a8b
Jinxu-Lin <jlin8272@uni.sydney.edu.au>
2025-01-14 15:29:37 +1100

    train

--- Classification/03_error.py
+++ Classification/03_error.py
@@ -74,34 +74,6 @@ def get_out_to_loss_grad(
         return (1 - ps).clone().detach().unsqueeze(-1)
 
 
-def get_dataloader(batch_size=256, num_workers=8, split='train', shuffle=False, augment=True):
-    if augment:
-        transforms = torchvision.transforms.Compose(
-                        [torchvision.transforms.RandomHorizontalFlip(),
-                         torchvision.transforms.RandomAffine(0),
-                         torchvision.transforms.ToTensor(),
-                         torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                                          (0.2023, 0.1994, 0.201))])
-    else:
-        transforms = torchvision.transforms.Compose([
-                         torchvision.transforms.ToTensor(),
-                         torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                                          (0.2023, 0.1994, 0.201))])
-        
-    is_train = (split == 'train')
-    dataset = torchvision.datasets.CIFAR10(root='/tmp/cifar/',
-                                           download=True,
-                                           train=is_train,
-                                           transform=transforms)
-
-    loader = torch.utils.data.DataLoader(dataset=dataset,
-                                         shuffle=shuffle,
-                                         batch_size=batch_size,
-                                         num_workers=num_workers)
-    
-    return loader
-
-
 def parseArgs():
 
     parser = argparse.ArgumentParser(
@@ -162,9 +134,13 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if args.dataset_split == 'train':
-        loader = get_dataloader(batch_size=args.batch_size, split='train')
+        loader = dataset_loader[args.dataset].get_train_loader(
+            args,
+        )
     else:
-        loader = get_dataloader(batch_size=args.batch_size, split='val', augment=False)
+        loader = dataset_loader[args.dataset].get_test_loader(
+            args,
+        )
 
     # load model
     num_classes = dataset_num_classes[args.dataset]
@@ -200,10 +176,9 @@ def main(args):
     for batch_idx, batch in enumerate(loader):
         print(f"{batch_idx}/{len(loader)}")
 
-        index_end = index_start + len(batch[0])
+        index_end = index_start + len(batch["input"])
 
-        # inputs, labels = batch["input"].to(device), batch["label"].to(device)
-        inputs, labels = batch[0].to(device), batch[1].to(device)
+        inputs, labels = batch["input"].to(device), batch["label"].to(device)
 
         loss_grads = get_out_to_loss_grad(model, func_weights, func_buffers, inputs, labels)
 
